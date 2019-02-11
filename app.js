c5c4b6b10ae4c4e6e93918027c665a8c6c1c423f
Piotr Kacala <pkacala@gog.com>
2019-02-11 17:37:22 +0100

    Init

---
+++ app.js
@@ -0,0 +1,40 @@
+jsPlumb.ready(function () {
+
+    var instance = window.jsp = jsPlumb.getInstance({
+        // default drag options
+        DragOptions: { cursor: 'pointer', zIndex: 2000 },
+        // the overlays to decorate each connection with.  note that the label overlay uses a function to generate the label text; in this
+        // case it returns the 'labelText' member that we set on each connection in the 'init' method below.
+        ConnectionOverlays: [
+            [ "Arrow", {
+                location: 1,
+                visible:true,
+                width:11,
+                length:11,
+                id:"ARROW",
+                events:{
+                    click:function() { alert("you clicked on the arrow overlay")}
+                }
+            } ],
+            [ "Label", {
+                location: 0.1,
+                id: "label",
+                cssClass: "aLabel",
+                events:{
+                    tap:function() { alert("hey"); }
+                }
+            }]
+        ],
+        // Container: "canvas"
+    });
+
+    instance.addEndpoint('a');
+    instance.addEndpoint('b');
+
+    // jsPlumb.connect({source: "a",target: "b",}, common);
+
+    var els = document.querySelectorAll(".note");
+    jsPlumb.draggable(els);
+
+    console.log('ji');
+});
